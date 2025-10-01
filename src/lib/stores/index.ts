ec2451566b7cd544ab5ad33950b7c94111f54e41
David Sewell <david.sewell@synechron.com>
2025-10-01 08:18:11 +0000

    chore: merge v0.6.32 into nexus chat v3 release branch

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -75,10 +75,12 @@ export const showArchivedChats = writable(false);
 export const showChangelog = writable(false);
 
 export const showControls = writable(false);
+export const showEmbeds = writable(false);
 export const showOverview = writable(false);
 export const showArtifacts = writable(false);
 export const showCallOverlay = writable(false);
 
+export const embed = writable(null);
 export const artifactCode = writable(null);
 
 export const temporaryChatEnabled = writable(false);
@@ -189,6 +191,7 @@ type Settings = {
 	notificationEnabled?: boolean;
 	highContrastMode?: boolean;
 	title?: TitleSettings;
+	showChatTitleInTab?: boolean;
 	splitLargeDeltas?: boolean;
 	chatDirection?: 'LTR' | 'RTL' | 'auto';
 	ctrlEnterToSend?: boolean;
@@ -285,7 +288,7 @@ type PromptSuggestion = {
 	title: [string, string];
 };
 
-type SessionUser = {
+export type SessionUser = {
 	permissions: any;
 	id: string;
 	email: string;
